fcfeb9db988ebf434251471e61c1a1c12da4f33a
Kasim <qosimmax@gmail.com>
2016-09-13 12:42:02 +0500

    expire time added

--- storage/storage.go
+++ storage/storage.go
@@ -1,28 +1,55 @@
 package storage
 
-import "errors"
+import (
+	"errors"
+	"sync"
+	"time"
+)
 
-type MemoryCache struct {
-	Cache map[string]interface{}
-}
+const (
+	// Default expiration time
+	DefaultExpiration time.Duration = 0
+	// nil value
+	NIL = "(nil)"
+)
 
 var (
 	errKeyHold = errors.New("Key holding the wrong kind of value")
 )
 
+type Item struct {
+	Object     interface{}
+	Expiration int64
+}
+
+type MemoryCache struct {
+	items map[string]Item
+	mu    sync.RWMutex
+}
+
 //Create new MemoryCache instance
 func New() *MemoryCache {
-	return &MemoryCache{Cache: make(map[string]interface{})}
+	return &MemoryCache{items: make(map[string]Item)}
+}
+
+// Sets the value at the specified key
+func (m *MemoryCache) Set(key string, value interface{}) (err error) {
+	m.items[key] = Item{
+		Object:     value,
+		Expiration: 0,
+	}
+
+	return
 }
 
 // Get the value of a key
 func (m *MemoryCache) Get(key string) (value string, err error) {
-	switch v := m.Cache[key].(type) {
+	switch v := m.items[key].Object.(type) {
 	case string:
 		value = v
 
 	case nil:
-		value = "nil"
+		value = NIL
 
 	default:
 		err = errKeyHold
@@ -31,22 +58,16 @@ func (m *MemoryCache) Get(key string) (value string, err error) {
 	return
 }
 
-// Sets the value at the specified key
-func (m *MemoryCache) Set(key string, value string) (err error) {
-	m.Cache[key] = value
-	return
-}
-
 // Set the string value of the field
 func (m *MemoryCache) HSet(key string, field string, value string) (err error) {
-	switch v := m.Cache[key].(type) {
+	switch v := m.items[key].Object.(type) {
 	case map[string]string:
 		v[field] = value
-		m.Cache[key] = v
+		m.Set(key, v)
 
 	case nil:
-		m.Cache[key] = map[string]string{
-			field: value}
+		m.Set(key, map[string]string{
+			field: value})
 
 	default:
 		err = errKeyHold
@@ -57,17 +78,17 @@ func (m *MemoryCache) HSet(key string, field string, value string) (err error) {
 
 // Get the value of a hash field stored at specified key
 func (m *MemoryCache) HGet(key string, field string) (value string, err error) {
-	switch v := m.Cache[key].(type) {
+	switch v := m.items[key].Object.(type) {
 	case map[string]string:
 		v2, ok := v[field]
 		if !ok {
-			value = "nil"
+			value = NIL
 			break
 		}
 		value = v2
 
 	case nil:
-		value = "nil"
+		value = NIL
 
 	default:
 		err = errKeyHold
@@ -78,7 +99,7 @@ func (m *MemoryCache) HGet(key string, field string) (value string, err error) {
 // LPush prepend one or multiple values to a list
 func (m *MemoryCache) LPush(key string, values ...string) (err error) {
 	var list []string
-	switch v := m.Cache[key].(type) {
+	switch v := m.items[key].Object.(type) {
 	case []string:
 		list = v
 
@@ -94,7 +115,7 @@ func (m *MemoryCache) LPush(key string, values ...string) (err error) {
 		list = append([]string{v}, list...)
 	}
 
-	m.Cache[key] = list
+	m.Set(key, list)
 	return
 
 }
