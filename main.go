38b114703df4cf8ed0571d9ab965ca36b3bc6c75
Kasim <qosimmax@gmail.com>
2016-09-16 15:02:31 +0500

    some functions added

--- main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-func main() {
-
-	m := make(map[string]interface{})
-	m["hi"] = "bla bla"
-	m["hello"] = 50
-
-}
--- storage/storage.go
+++ storage/storage.go
@@ -2,19 +2,17 @@ package storage
 
 import (
 	"errors"
-	"sync"
 	"time"
 )
 
 const (
 	// Default expiration time
 	DefaultExpiration time.Duration = -1
-	// nil value
-	NIL = "(nil)"
 )
 
 var (
-	errKeyHold = errors.New("Key holding the wrong kind of value")
+	errKeyHold   = errors.New("Key holding the wrong kind of value")
+	ErrNullValue = errors.New("Null value")
 )
 
 type Item struct {
@@ -23,13 +21,14 @@ type Item struct {
 }
 
 type MemoryCache struct {
-	items map[string]Item
-	mu    sync.RWMutex
+	items   map[string]Item
+	expires map[string]bool
+	//mu    sync.RWMutex
 }
 
 //Create new MemoryCache instance
 func New() *MemoryCache {
-	return &MemoryCache{items: make(map[string]Item)}
+	return &MemoryCache{items: make(map[string]Item), expires: make(map[string]bool)}
 }
 
 // Sets the value at the specified key
@@ -49,7 +48,7 @@ func (m *MemoryCache) Get(key string) (value string, err error) {
 		value = v
 
 	case nil:
-		value = NIL
+		err = ErrNullValue
 
 	default:
 		err = errKeyHold
@@ -59,9 +58,11 @@ func (m *MemoryCache) Get(key string) (value string, err error) {
 }
 
 // Remove the specified keys.
-func (m *MemoryCache) Remove(key string) {
+func (m *MemoryCache) Del(key string) bool {
+	_, ok := m.items[key]
 	delete(m.items, key)
-	return
+	delete(m.expires, key)
+	return ok
 }
 
 // Set expiration time for specified key
@@ -74,6 +75,7 @@ func (m *MemoryCache) SetTTL(key string, d time.Duration) {
 	value := m.items[key]
 	value.Expiration = int64(e)
 	m.items[key] = value
+	m.expires[key] = true
 	return
 }
 
@@ -101,13 +103,50 @@ func (m *MemoryCache) HGet(key string, field string) (value string, err error) {
 	case map[string]string:
 		v2, ok := v[field]
 		if !ok {
-			value = NIL
+			err = ErrNullValue
 			break
 		}
 		value = v2
 
 	case nil:
-		value = NIL
+		err = ErrNullValue
+
+	default:
+		err = errKeyHold
+	}
+	return
+}
+
+// Get all the fields and values stored in a hash at specified key
+func (m *MemoryCache) HGetAll(key string) (values []string, err error) {
+	switch v := m.items[key].Object.(type) {
+	case map[string]string:
+		for k := range v {
+			values = append(values, k, v[k])
+		}
+	case nil:
+		err = ErrNullValue
+
+	default:
+		err = errKeyHold
+	}
+	return
+}
+
+func (m *MemoryCache) HDel(key string, fields ...string) (n int, err error) {
+	switch v := m.items[key].Object.(type) {
+	case map[string]string:
+		for _, f := range fields {
+			_, ok := v[f]
+			if ok {
+				n++
+				delete(v, f)
+			}
+		}
+		m.Set(key, v)
+
+	case nil:
+		err = ErrNullValue
 
 	default:
 		err = errKeyHold
@@ -140,7 +179,7 @@ func (m *MemoryCache) LPush(key string, values ...string) (err error) {
 }
 
 // Get element from a list by its index
-func (m *MemoryCache) Lindex(key string, i int) (value string) {
+func (m *MemoryCache) Lindex(key string, i int) (value string, err error) {
 	switch v := m.items[key].Object.(type) {
 	case []string:
 		n := len(v)
@@ -152,11 +191,66 @@ func (m *MemoryCache) Lindex(key string, i int) (value string) {
 			value = v[n+i]
 			break
 		}
-		value = NIL
+		err = ErrNullValue
+
+	case nil:
+		err = ErrNullValue
 
 	default:
-		value = NIL
+		err = errKeyHold
 
+	}
+	return
+}
+
+// Get the length of the list stored at key
+func (m *MemoryCache) Llen(key string) (n int, err error) {
+	switch v := m.items[key].Object.(type) {
+	case []string:
+		return len(v), nil
+
+	case nil:
+		err = ErrNullValue
+
+	default:
+		err = errKeyHold
+	}
+
+	return
+}
+
+// Remove and get the first element in a list
+func (m *MemoryCache) LPop(key string) (value string, err error) {
+	switch v := m.items[key].Object.(type) {
+	case []string:
+		if len(v) < 1 {
+			err = ErrNullValue
+			break
+		}
+		value = v[0]
+		m.Set(key, v[1:])
+
+	case nil:
+		err = ErrNullValue
+
+	default:
+		err = errKeyHold
+
+	}
+	return
+}
+
+// Check for key expire
+func (m *MemoryCache) IsExpire(key string) bool {
+	now := time.Now().Unix()
+	_, ok := m.expires[key]
+	return ok && (now > m.items[key].Expiration)
+}
+
+// Get expire key list
+func (m *MemoryCache) ExpireList() (list []string) {
+	for key := range m.expires {
+		list = append(list, key)
 	}
 	return
 }
